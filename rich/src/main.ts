2f56ebc9eeca4de90f71bff224ce845f69f10836
carlos676767 <carlitobr730@gmail.com>
2024-05-13 08:23:19 -0700

    ok

--- rich/src/main.ts
+++ rich/src/main.ts
@@ -1,17 +1,43 @@
 const textNrea = document.querySelector("textarea") as HTMLTextAreaElement;
+const itensIConfiguracoes = document.querySelectorAll("i") 
+let contadorFontTamanho: number = 0;
+
+interface Configuracoes {
+  textNegrito: (text: HTMLTextAreaElement,  tamanhoFont: string) => void
+}
+
+const config: Configuracoes = {
+  textNegrito: (text: HTMLTextAreaElement, tamanhoFont: string) => {text.style.fontSize = tamanhoFont},
+};
+
+const { textNegrito } = config;
+
 
 const pegarInputValue = (): void => {
   textNrea.addEventListener("input", () => {
     console.log(textNrea.value);
-    localStorage.setItem("dadosSalvos", textNrea.value)
-  })
+    localStorage.setItem("dadosSalvos", textNrea.value);
+  });
 };
 
-
-const salvarDadosInputLocalStorage = () : void => {
+const salvarDadosInputLocalStorage = (): void => {
   const recuperarDados = localStorage.getItem("dadosSalvos");
   textNrea.value = `${recuperarDados}`;
 };
 
-salvarDadosInputLocalStorage()
+
+const isimpar = (): void => {
+  ++contadorFontTamanho;
+  if (contadorFontTamanho % 2 !== 0) {
+    textNegrito(textNrea, "30px");
+  } else {
+    textNegrito(textNrea, "15px");
+  }
+};
+
+itensIConfiguracoes[0].addEventListener("click", () => {
+ isimpar();
+});
+
+salvarDadosInputLocalStorage();
 pegarInputValue();
