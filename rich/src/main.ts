fb1c90aa1f9488ef823adc384b9d65bbb545327e
carlos676767 <carlitobr730@gmail.com>
2024-05-13 07:35:48 -0700

    local storage ok.

--- rich/src/main.ts
+++ rich/src/main.ts
@@ -3,13 +3,15 @@ const textNrea = document.querySelector("textarea") as HTMLTextAreaElement;
 const pegarInputValue = (): void => {
   textNrea.addEventListener("input", () => {
     console.log(textNrea.value);
+    localStorage.setItem("dadosSalvos", textNrea.value)
   })
 };
 
 
-const salvarDadosInputLocalStorage = () => {
-  
-}
+const salvarDadosInputLocalStorage = () : void => {
+  const recuperarDados = localStorage.getItem("dadosSalvos");
+  textNrea.value = `${recuperarDados}`;
+};
 
-
-pegarInputValue()
+salvarDadosInputLocalStorage()
+pegarInputValue();
